363723be6f8f743686f1c972eb91074065f125df
Charvi-45 <f20250835@pilani.bits-pilani.ac.in>
2026-09-14 08:30:52 +0000

    Added BigBird mask implementation

---
+++ src/attention.py
@@ -0,0 +1,23 @@
+import math
+from utils.attention_utils import matmul, transpose, softmax
+import torch
+device="cuda"
+def dense(
+    
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    mask: torch.Tensor| None = None,
+) -> torch.Tensor:
+
+
+   d = q.shape[-1]
+   scale = 1.0 / math.sqrt(d)
+   scores = torch.matmul(q, k.transpose(-2, -1)) * scale
+
+   scores = scores.masked_fill(mask==0, float("-inf"))
+   weights = torch.softmax(scores, dim=-1)
+
+   return torch.matmul(weights, v)
+
+   
