2883a2d4455a7690c07ede3c3ae24b76e5e25cca
Charvi-45 <f20250835@pilani.bits-pilani.ac.in>
2026-09-15 17:30:35 +0000

    Implement mask

--- src/slidingmasks.py
+++ src/slidingmasks.py
@@ -1,12 +1,15 @@
-
 import torch
-device = torch.device("cuda")
-def sliding_win(seq_len:int,w:int,device=device):
 
-  mask = torch.zeros(seq_len,seq_len)
-  for i in range(seq_len):
-    for j in range(seq_len):
-        if max(0, i-w+1) <= j <= i:
-            mask[i, j] = 1
+def sliding_win(seq_len: int, w: int, device):
+    mask = torch.zeros(
+        seq_len,
+        seq_len,
+        dtype=torch.bool,
+        device=device
+    )
 
-  return mask
+    for i in range(seq_len):
+        start = max(0, i - w + 1)
+        mask[i, start:i + 1] = True
+
+    return mask
