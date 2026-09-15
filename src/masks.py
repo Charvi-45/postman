2c33e81e77b6ca98941a4f42334a3510f1d1d957
Charvi-45 <f20250835@pilani.bits-pilani.ac.in>
2026-09-15 16:51:34 +0000

    Added Colab notebook

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,14 @@
-
 import torch
 def mask(q):
-  seq_len = q.shape[-2]
-  #causal mask
 
-  mask=torch.tril(torch.ones(seq_len, seq_len))
-  return mask
+    seq_len = q.shape[-2]
+
+    mask = torch.tril(
+        torch.ones(
+            seq_len,
+            seq_len,
+            device=q.device
+        )
+    )
+
+    return mask
